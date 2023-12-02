e9abe1e9df0ddf06b0f3d77a234344d836ae840d
ausprouty <bob.prouty@powertochange.org.au>
2023-12-03 08:36:04 +1100

    rewrote routes

--- routes.php
+++ routes.php
@@ -12,56 +12,56 @@ require_once  __DIR__.'/configuration/class-autoload.inc.php';
 require_once __DIR__.'/includes/writeLog.php';
 
 //API
-get(WEB_ROOT . 'api/ask/$languageCodeHL', 'api/askQuestions.php');
-get(WEB_ROOT . 'api/bibles/$languageCodeHL', 'api/biblesForLanguage.php');
+get(WEB_ROOT . 'api/ask/$languageCodeHL', 'App/API/askQuestions.php');
+get(WEB_ROOT . 'api/bibles/$languageCodeHL', 'App/API/biblesForLanguage.php');
 get(WEB_ROOT . 'api/bibles/dbs/next/$languageCodeHL','api/bibleForDbsNext.php');
-get(WEB_ROOT . 'api/bibles/text/$languageCodeHL', 'api/biblesForLanguageTextOnly.php');
+get(WEB_ROOT . 'api/bibles/text/$languageCodeHL', 'App/API/biblesForLanguageTextOnly.php');
 
-get(WEB_ROOT . 'api/content/available/$languageCodeHL1/$languageCodeHL2', 'api/contentAvailable.php');
-get(WEB_ROOT . 'api/createQrCode', 'api/createQrCode.php');
+get(WEB_ROOT . 'api/content/available/$languageCodeHL1/$languageCodeHL2', 'App/API/contentAvailable.php');
+get(WEB_ROOT . 'api/createQrCode', 'App/API/createQrCode.php');
 
-get(WEB_ROOT . 'api/dbs/drupal/view/$lesson/$languageCodeHL1/$languageCodeHL2', 'api/dbsBilingualViewDrupal.php');
-get(WEB_ROOT . 'api/dbs/languages', 'api/dbsLanguageOptions.php');
-get(WEB_ROOT . 'api/dbs/pdf/$lesson/$languageCodeHL1/$languageCodeHL2', 'api/dbsBilingualPdf.php');
-get(WEB_ROOT . 'api/dbs/studies', 'api/dbsStudyOptions.php');
-get(WEB_ROOT . 'api/dbs/studies/$languageCodeHL1', 'api/dbsStudyOptions.php');
-get(WEB_ROOT . 'api/dbs/view/$lesson/$languageCodeHL1', 'api/dbsMonolingualView.php');
-get(WEB_ROOT . 'api/dbs/view/$lesson/$languageCodeHL1/$languageCodeHL2', 'api/dbsBilingualView.php');
-get(WEB_ROOT . 'api/dbs/$lesson/$languageCodeHL', 'api/dbs.php');
-get(WEB_ROOT . 'api/followingjesus/segments/$languageCodeHL', 'api/followingJesusOptions.php');
+get(WEB_ROOT . 'api/dbs/drupal/view/$lesson/$languageCodeHL1/$languageCodeHL2', 'App/API/dbsBilingualViewDrupal.php');
+get(WEB_ROOT . 'api/dbs/languages', 'App/API/dbsLanguageOptions.php');
+get(WEB_ROOT . 'api/dbs/pdf/$lesson/$languageCodeHL1/$languageCodeHL2', 'App/API/dbsBilingualPdf.php');
+get(WEB_ROOT . 'api/dbs/studies', 'App/API/dbsStudyOptions.php');
+get(WEB_ROOT . 'api/dbs/studies/$languageCodeHL1', 'App/API/dbsStudyOptions.php');
+get(WEB_ROOT . 'api/dbs/view/$lesson/$languageCodeHL1', 'App/API/dbsMonolingualView.php');
+get(WEB_ROOT . 'api/dbs/view/$lesson/$languageCodeHL1/$languageCodeHL2', 'App/API/dbsBilingualView.php');
+get(WEB_ROOT . 'api/dbs/$lesson/$languageCodeHL', 'App/API/dbs.php');
+get(WEB_ROOT . 'api/followingjesus/segments/$languageCodeHL', 'App/API/followingJesusOptions.php');
 
-get(WEB_ROOT . 'api/gospel/languages', 'api/gospelLanguageOptions.php');
-get(WEB_ROOT . 'api/gospel/view/$page', 'api/gospelPage.php');
+get(WEB_ROOT . 'api/gospel/languages', 'App/API/gospelLanguageOptions.php');
+get(WEB_ROOT . 'api/gospel/view/$page', 'App/API/gospelPage.php');
 
-get(WEB_ROOT . 'api/hindi/languages', 'api/hindiLanguageOptions.php');
+get(WEB_ROOT . 'api/hindi/languages', 'App/API/hindiLanguageOptions.php');
 
-get(WEB_ROOT . 'api/jvideo/questions/$languageCodeHL', 'api/jVideoQuestionsMonolingual.php');
-get(WEB_ROOT . 'api/jvideo/questions/$languageCodeHL1/$languageCodeHL2', 'api/jVideoQuestionsBilingual.php');
-get(WEB_ROOT . 'api/jvideo/segments/$languageCodeHL', 'api/jVideoSegments.php');
+get(WEB_ROOT . 'api/jvideo/questions/$languageCodeHL', 'App/API/jVideoQuestionsMonolingual.php');
+get(WEB_ROOT . 'api/jvideo/questions/$languageCodeHL1/$languageCodeHL2', 'App/API/jVideoQuestionsBilingual.php');
+get(WEB_ROOT . 'api/jvideo/segments/$languageCodeHL', 'App/API/jVideoSegments.php');
 
-get(WEB_ROOT . 'api/language/$languageCodeHL', 'api/languageDetails.php');
-get(WEB_ROOT . 'api/language/languageCodeJF/$languageCodeHL', 'api/languageCodeJF.php');
-get(WEB_ROOT . 'api/language/languageCodeJFFollowingJesus/$languageCodeHL', 'api/languageCodeJFFollowingJesus.php');
-get(WEB_ROOT . 'api/languages/country/$countryCode', 'api/languagesForCountry.php');
+get(WEB_ROOT . 'api/language/$languageCodeHL', 'App/API/languageDetails.php');
+get(WEB_ROOT . 'api/language/languageCodeJF/$languageCodeHL', 'App/API/languageCodeJF.php');
+get(WEB_ROOT . 'api/language/languageCodeJFFollowingJesus/$languageCodeHL', 'App/API/languageCodeJFFollowingJesus.php');
+get(WEB_ROOT . 'api/languages/country/$countryCode', 'App/API/languagesForCountry.php');
 
-get(WEB_ROOT . 'api/leadership/studies', 'api/leadershipStudyOptions.php');
-get(WEB_ROOT . 'api/leadership/studies/$languageCodeHL1', 'api/leadershipStudyOptions.php');
-get(WEB_ROOT . 'api/leadership/view/$lesson/$languageCodeHL1', 'api/leadershipMonolingualView.php');
-get(WEB_ROOT . 'api/leadership/view/$lesson/$languageCodeHL1/$languageCodeHL2', 'api/leadershipBilingualView.php');
-get(WEB_ROOT . 'api/leadership/pdf/$lesson/$languageCodeHL1/$languageCodeHL2', 'api/leadershipBilingualPdf.php');
+get(WEB_ROOT . 'api/leadership/studies', 'App/API/leadershipStudyOptions.php');
+get(WEB_ROOT . 'api/leadership/studies/$languageCodeHL1', 'App/API/leadershipStudyOptions.php');
+get(WEB_ROOT . 'api/leadership/view/$lesson/$languageCodeHL1', 'App/API/leadershipMonolingualView.php');
+get(WEB_ROOT . 'api/leadership/view/$lesson/$languageCodeHL1/$languageCodeHL2', 'App/API/leadershipBilingualView.php');
+get(WEB_ROOT . 'api/leadership/pdf/$lesson/$languageCodeHL1/$languageCodeHL2', 'App/API/leadershipBilingualPdf.php');
 
-get(WEB_ROOT . 'api/life_principles/studies', 'api/lifeStudyOptions.php');
-get(WEB_ROOT . 'api/life_principles/studies/$languageCodeHL1', 'api/lifeStudyOptions.php');
-get(WEB_ROOT . 'api/life_principles/view/$lesson/$languageCodeHL1', 'api/lifeMonolingualView.php');
-get(WEB_ROOT . 'api/life_principles/view/$lesson/$languageCodeHL1/$languageCodeHL2', 'api/lifeBilingualView.php');
-get(WEB_ROOT . 'api/life_principles/pdf/$lesson/$languageCodeHL1/$languageCodeHL2', 'api/lifeBilingualPdf.php');
+get(WEB_ROOT . 'api/life_principles/studies', 'App/API/lifeStudyOptions.php');
+get(WEB_ROOT . 'api/life_principles/studies/$languageCodeHL1', 'App/API/lifeStudyOptions.php');
+get(WEB_ROOT . 'api/life_principles/view/$lesson/$languageCodeHL1', 'App/API/lifeMonolingualView.php');
+get(WEB_ROOT . 'api/life_principles/view/$lesson/$languageCodeHL1/$languageCodeHL2', 'App/API/lifeBilingualView.php');
+get(WEB_ROOT . 'api/life_principles/pdf/$lesson/$languageCodeHL1/$languageCodeHL2', 'App/API/lifeBilingualPdf.php');
 
-get(WEB_ROOT . 'api/video/code/$title/$languageCodeHL', 'api/videoCodeFromTitle.php');
-get(WEB_ROOT . 'api/videocode/followingjesus/$languageCodeHL' , 'api/videoCodeForFollowingJesus.php');
+get(WEB_ROOT . 'api/video/code/$title/$languageCodeHL', 'App/API/videoCodeFromTitle.php');
+get(WEB_ROOT . 'api/videocode/followingjesus/$languageCodeHL' , 'App/API/videoCodeForFollowingJesus.php');
 
-post(WEB_ROOT .'api/passage/text', 'api/passageForBible.php');
+post(WEB_ROOT .'api/passage/text', 'App/API/passageForBible.php');
 
-post(WEB_ROOT . 'api/secure/bibles/weight/change', 'api/secure/bibleWeightChange.php');
+post(WEB_ROOT . 'api/secure/bibles/weight/change', 'App/API/secure/bibleWeightChange.php');
 
 // Index
 
@@ -109,58 +109,58 @@ if (WEB_ROOT == '/mylanguage-api/'){
 
 
     // TESTS
-    get(WEB_ROOT . 'tests/createQrCode', 'tests/createQrCode.php');
-    get(WEB_ROOT . 'test', 'tests/test.php');
-    get(WEB_ROOT . 'test/bengali', 'tests/bengaliTest.php');
+    get(WEB_ROOT . 'tests/createQrCode',  'App/Tests/createQrCode.php');
+    get(WEB_ROOT . 'test',  'App/Tests/test.php');
+    get(WEB_ROOT . 'test/bengali',  'App/Tests/bengaliTest.php');
     //  Web Access
-    get(WEB_ROOT . 'webpage', 'tests/webpage.php');
+    get(WEB_ROOT . 'webpage',  'App/Tests/webpage.php');
 
     // word
-    get(WEB_ROOT . 'test/word/passage/$externalId','tests/canGetBibleWordPassageFromExternalId.php');
+    get(WEB_ROOT . 'test/word/passage/$externalId', 'App/Tests/canGetBibleWordPassageFromExternalId.php');
 
     // Bible Brain
-    get(WEB_ROOT . 'test/biblebrain/language', 'tests/canGetBibleBrainLanguageDetails.php');
-    get(WEB_ROOT . 'test/biblebrain/bible/default', 'tests/canGetBestBibleFromBibleBrain.php');
-    get(WEB_ROOT . 'test/biblebrain/bible/formats', 'tests/canGetBibleBrainBibleFormatTypes.php');
-    get(WEB_ROOT . 'test/biblebrain/passage/json', 'tests/canGetBibleBrainPassageTextJson.php');
+    get(WEB_ROOT . 'test/biblebrain/language',  'App/Tests/canGetBibleBrainLanguageDetails.php');
+    get(WEB_ROOT . 'test/biblebrain/bible/default',  'App/Tests/canGetBestBibleFromBibleBrain.php');
+    get(WEB_ROOT . 'test/biblebrain/bible/formats',  'App/Tests/canGetBibleBrainBibleFormatTypes.php');
+    get(WEB_ROOT . 'test/biblebrain/passage/json',  'App/Tests/canGetBibleBrainPassageTextJson.php');
 
-    get(WEB_ROOT . 'test/biblebrain/passage/formatted','tests/canGetBibleBrainPassageTextFormatted.php');
-    get(WEB_ROOT . 'test/biblebrain/passage/usx','tests/canGetBibleBrainPassageTextUsx.php');
-    get(WEB_ROOT . 'test/biblebrain/languages/country', 'tests/canGetLanguagesForCountryCode.php');
+    get(WEB_ROOT . 'test/biblebrain/passage/formatted', 'App/Tests/canGetBibleBrainPassageTextFormatted.php');
+    get(WEB_ROOT . 'test/biblebrain/passage/usx', 'App/Tests/canGetBibleBrainPassageTextUsx.php');
+    get(WEB_ROOT . 'test/biblebrain/languages/country',  'App/Tests/canGetLanguagesForCountryCode.php');
 
     // Bible Gateway
-    get(WEB_ROOT . 'test/biblegateway', 'tests/canGetBibleGatewayPassage.php');
+    get(WEB_ROOT . 'test/biblegateway',  'App/Tests/canGetBibleGatewayPassage.php');
 
     //YouVersion
-    get(WEB_ROOT . 'test/youversion/link', 'tests/canGetBibleYouVersionLink.php');
+    get(WEB_ROOT . 'test/youversion/link',  'App/Tests/canGetBibleYouVersionLink.php');
 
     // DBS
-    get(WEB_ROOT . 'test/dbs/translation', 'tests/canGetDBSTranslation.php');
-    get(WEB_ROOT . 'test/dbs/bilingual', 'tests/canMakeStandardBilingualDBS.php');
-    get(WEB_ROOT . 'test/dbs/pdf', 'tests/canPrintDbsPdf.php');
+    get(WEB_ROOT . 'test/dbs/translation',  'App/Tests/canGetDBSTranslation.php');
+    get(WEB_ROOT . 'test/dbs/bilingual',  'App/Tests/canMakeStandardBilingualDBS.php');
+    get(WEB_ROOT . 'test/dbs/pdf',  'App/Tests/canPrintDbsPdf.php');
 
 
-    //Bibles
-    get (WEB_ROOT. 'test/bibles/best', 'tests/canGetBestBibleByLanguageCodeHL.php');
-    get (WEB_ROOT. 'test/passage/select', 'tests/passageSelectControllerTest.php');
-    get(WEB_ROOT . 'test/bible', 'tests/biblePassageControllerTest.php');
+    /Bibles
+    get (WEB_ROOT. 'test/bibles/best',  'App/Tests/canGetBestBibleByLanguageCodeHL.php');
+    get (WEB_ROOT. 'test/passage/select',  'App/Tests/passageSelectControllerTest.php');
+    get(WEB_ROOT . 'test/bible',  'App/Tests/biblePassageControllerTest.php');
 
-    //Database
-    get(WEB_ROOT . 'test/language/hl', 'tests/canGetLanguageFromHL.php');
-
-
+    /Database
+    get(WEB_ROOT . 'test/language/hl',  'App/Tests/canGetLanguageFromHL.php');
 
 
 
-    get(WEB_ROOT . 'test/bible/reference/info', 'tests/CanCreateBibleReferenceInfo.php');
-    get(WEB_ROOT . 'test/passage/select', 'tests/canSelectBiblePassageFromDatabaseOrExternal.php');
 
-    get(WEB_ROOT . 'test/passage/stored', 'tests/canSeePassageStored.php');
+
+    get(WEB_ROOT . 'test/bible/reference/info',  'App/Tests/CanCreateBibleReferenceInfo.php');
+    get(WEB_ROOT . 'test/passage/select',  'App/Tests/canSelectBiblePassageFromDatabaseOrExternal.php');
+
+    get(WEB_ROOT . 'test/passage/stored',  'App/Tests/canSeePassageStored.php');
 
 }
-// any can be used for GETs or POSTs
+/ any can be used for GETs or POSTs
 
-// For GET or POST
-// The 404.php which is inside the tests folder will be called
-// The 404.php has access to $_GET and $_POST
+/ For GET or POST
+/ The 404.php which is inside the tests folder will be called
+/ The 404.php has access to $_GET and $_POST
 any('/404','views/404.php');
